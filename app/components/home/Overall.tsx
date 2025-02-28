e28f40c5b125664ba529e233db8888baea1d51ce
Luis Angelo Fruto <luisfruto7@gmail.com>
2025-02-28 17:46:41 +0800

    chore

--- app/components/home/Overall.tsx
+++ app/components/home/Overall.tsx
@@ -208,7 +208,7 @@ const Overall = ({ scores }: OverallProps) => {
           Disclaimer: The scores are not final and may be subject to changes.
         </p>
         <p className="text-xs text-center italic">
-          Last Updated: February 28 - 1:05 PM
+          Last Updated: February 28 - 4:54 PM
         </p>
       </div>
     </section>
